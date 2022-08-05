3fdfc1c2586ca28979490d048bc708affb9bfa01
Luis Eduardo <luiseduardosilva189@gmail.com>
2022-08-05 11:30:08 -0300

    start setup

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require("express")
+app = express();
+const expressLayouts = require("express-ejs-layouts")
+
+const indexRouter = require("./routes/index");
+
+app.set("view engine", "ejs");
+app.set("views",__dirname + "/views");
+app.set("layout","layouts/layout");
+app.use(expressLayouts);
+app.use(express.static("public"));
+
+app.use("/",indexRouter)
+
+
+
+
+app.listen(process.env.PORT || 3040)
+console.log("server up")
+
+
